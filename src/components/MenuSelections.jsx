86e528d737fff97656386fb70d798f3ac811e6b0
primatara42 <primatara42@gmail.com>
2024-09-16 09:56:48 +0700

    fix(favorite menu section): fix position of dropdown menu and paragraph

--- src/components/MenuSelections.jsx
+++ src/components/MenuSelections.jsx
@@ -7,13 +7,13 @@ function MenuSelections() {
       <h1 className="text-secondary text-xl text-center font-bold mb-5 md:text-3xl">
         Menu <span className="text-primary">Selections</span>
       </h1>
-      <div>
-        <p className="text-sm text-justify mb-3 sm:text-base md:text-lg">
+      <div className="md:flex md:justify-between md:items-center">
+        <p className="text-sm text-justify mb-3 sm:text-base md:text-lg md:w-96 lg:w-[80%]">
           Explore our wide range of delicious treats and desserts, from classic
           cakes to delightful pastries.
         </p>
         <div className="flex justify-end mb-3">
-          <select className="bg-primary text-white rounded-full px-3 py-2 md:text-lg">
+          <select className="bg-primary text-white rounded-full px-3 py-2 text-sm md:text-lg">
             <option value="all">All</option>
             <option value="cake">Cake</option>
             <option value="donut">Donut</option>
@@ -22,7 +22,25 @@ function MenuSelections() {
           </select>
         </div>
       </div>
-      <div className="card_container flex flex-wrap justify-evenly gap-y-5 gap-x-10">
+      <div className="card_container flex flex-wrap justify-between gap-y-10 gap-x-5">
+        <div className="card bg-white p-5 rounded-xl shadow-xl font-semibold sm:max-w-72">
+            <img src={FavoriteMenu} className="w-24 h-24 object-cover rounded-full mb-3 sm:w-36 sm:h-36"/>
+            <h2 className="text-base sm:text-lg">Cheesecake Speculoos</h2>
+            <p className="font-normal text-sm text-justify mb-3 sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus repudiandae reiciendis, rerum magnam expedita.</p>
+        <div className="text-primary text-base sm:text-lg flex justify-between items-center">
+            <p>Rp 25.000</p>
+            <button className="bg-white border-primary border-2 rounded-full p-2">Add to Cart</button>
+        </div>
+        </div>
+        <div className="card bg-white p-5 rounded-xl shadow-xl font-semibold sm:max-w-72">
+            <img src={FavoriteMenu} className="w-24 h-24 object-cover rounded-full mb-3 sm:w-36 sm:h-36"/>
+            <h2 className="text-base sm:text-lg">Cheesecake Speculoos</h2>
+            <p className="font-normal text-sm text-justify mb-3 sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus repudiandae reiciendis, rerum magnam expedita.</p>
+        <div className="text-primary text-base sm:text-lg flex justify-between items-center">
+            <p>Rp 25.000</p>
+            <button className="bg-white border-primary border-2 rounded-full p-2">Add to Cart</button>
+        </div>
+        </div>
         <div className="card bg-white p-5 rounded-xl shadow-xl font-semibold sm:max-w-72">
             <img src={FavoriteMenu} className="w-24 h-24 object-cover rounded-full mb-3 sm:w-36 sm:h-36"/>
             <h2 className="text-base sm:text-lg">Cheesecake Speculoos</h2>
